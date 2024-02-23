c1d5713bdabc15e8974862006b8881f6a2ac44cc
macdeo <akinbiyi.adeola@gmail.com>
2024-02-23 23:55:13 +0100

    Added Wind, Humidity and Visibility

--- lib/home.dart
+++ lib/home.dart
@@ -15,16 +15,18 @@ class _HomeState extends State<Home> {
   late List<IndividualBar> chartData;
   late String currentTemp;
   late List<double> currentDataTemp;
+  late List<double> totalHumidity;
+  late List<double> totalVisibility;
+  late List<double> totalWind;
+  late double totalVisibilityKM;
 
   String api =
-      'https://api.open-meteo.com/v1/forecast?latitude=49.4295&longitude=1.0791&hourly=temperature_2m';
+      'https://api.open-meteo.com/v1/gfs?latitude=52.52&longitude=13.41&hourly=temperature_2m,relative_humidity_2m,visibility,wind_speed_10m';
 
   Future<Map<String, List<String>>> getData = GetData().getData();
 
   Future<Map<String, List<String>>> getCurrentWeather =
       GetData().getCurrentWeather();
-
-  // String temperature = GetData().temperature();
 
   @override
   Widget build(BuildContext context) {
@@ -32,8 +34,9 @@ class _HomeState extends State<Home> {
     String todayDate = DateFormat('EEEE, d MMMM').format(date);
 
     return Scaffold(
+      backgroundColor: const Color(0xFFfae143),
       appBar: AppBar(
-        backgroundColor: Colors.purple.shade50,
+        backgroundColor: const Color(0xFFfae143),
         title: const Text('Weather App'),
       ),
       body: SafeArea(
@@ -52,7 +55,7 @@ class _HomeState extends State<Home> {
                     child: Text(
                   todayDate.toString(),
                   style: const TextStyle(
-                      color: Colors.white, fontWeight: FontWeight.w500),
+                      color: Color(0xFFfae143), fontWeight: FontWeight.w500),
                 )),
               ),
             ),
@@ -69,19 +72,33 @@ class _HomeState extends State<Home> {
                           .map<double>((temp) => double.parse(temp))
                           .toList();
 
+                      totalHumidity = snapshot.data!['totalHumidity']!
+                          .map<double>((temp) => double.parse(temp))
+                          .toList();
+
+                      totalVisibility = snapshot.data!['totalVisibility']!
+                          .map<double>((temp) => double.parse(temp))
+                          .toList();
+
+                      totalWind = snapshot.data!['totalWind']!
+                          .map<double>((temp) => double.parse(temp))
+                          .toList();
+
+                      totalVisibilityKM = (totalVisibility[0] / 1000);
+
                       if ((currentDataTemp[0] <= 0)) {
-                        currentTemp = 'Freezing 🧊';
+                        currentTemp = 'Freezing';
                       } else if ((currentDataTemp[0] > 0) &&
                           (currentDataTemp[0] <= 10)) {
-                        currentTemp = 'Cold ❄️';
+                        currentTemp = 'Cold';
                       } else if ((currentDataTemp[0] > 10) &&
                           (currentDataTemp[0] <= 20)) {
-                        currentTemp = 'Warm 🌤️';
+                        currentTemp = 'Warm️';
                       } else if ((currentDataTemp[0] > 20) &&
                           (currentDataTemp[0] <= 40)) {
-                        currentTemp = 'Sunny ☀️';
+                        currentTemp = 'Sunny';
                       } else {
-                        currentTemp = 'Extremely Hot 🥵';
+                        currentTemp = 'Extremely Hot';
                       }
                     }
                     return Column(
@@ -94,9 +111,90 @@ class _HomeState extends State<Home> {
                         Text(
                           '${currentDataTemp[0].toString().substring(0, 1)}°',
                           style: const TextStyle(
-                              fontFamily: 'Roboto',
                               fontWeight: FontWeight.bold,
-                              fontSize: 100),
+                              fontSize: 120),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 30.0),
+                          child: Container(
+                            height: MediaQuery.sizeOf(context).height * 0.15,
+                            width: MediaQuery.sizeOf(context).width * 0.85,
+                            decoration: BoxDecoration(
+                                color: Colors.black,
+                                borderRadius: BorderRadius.circular(10)),
+                            child: Center(
+                              child: Table(
+                                children: [
+                                  const TableRow(children: <Widget>[
+                                    Center(
+                                        child: Icon(
+                                      Icons.air_outlined,
+                                      color: Color(0xFFfae143),
+                                      size: 50,
+                                    )),
+                                    Center(
+                                        child: Icon(
+                                      Icons.water_drop_outlined,
+                                      color: Color(0xFFfae143),
+                                      size: 50,
+                                    )),
+                                    Center(
+                                        child: Icon(
+                                      Icons.visibility_outlined,
+                                      color: Color(0xFFfae143),
+                                      size: 50,
+                                    )),
+                                  ]),
+                                  TableRow(children: <Widget>[
+                                    Center(
+                                        child: Text(
+                                          '${totalWind[0].round().toString()}km/h',
+                                      style: const TextStyle(
+                                          color: Color(0xFFfae143),
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold),
+                                    )),
+                                    Center(
+                                        child: Text(
+                                          '${totalHumidity[0].round().toString()}%',
+                                          style: const TextStyle(
+                                              color: Color(0xFFfae143),
+                                              fontSize: 20,
+                                              fontWeight: FontWeight.bold),
+                                        )),
+                                    Center(
+                                        child: Text(
+                                          '${totalVisibilityKM.toStringAsFixed(1).toString()}km',
+                                          style: const TextStyle(
+                                              color: Color(0xFFfae143),
+                                              fontSize: 20,
+                                              fontWeight: FontWeight.bold),
+                                        )),
+                                  ]),
+                                  const TableRow(children: <Widget>[
+                                    Center(
+                                        child: Text(
+                                          'Wind',
+                                          style: TextStyle(
+                                              color: Color(0xFFfae143),),
+                                        )),
+                                    Center(
+                                        child: Text(
+                                          'Humidity',
+                                          style: TextStyle(
+                                            color: Color(0xFFfae143),),
+                                        )),
+                                    Center(
+                                        child: Text(
+                                          'Visibility',
+                                          style: TextStyle(
+                                            color: Color(0xFFfae143),),
+                                        )),
+                                  ]),
+                                ],
+                              ),
+                            ),
+                          ),
                         )
                       ],
                     );
@@ -104,57 +202,61 @@ class _HomeState extends State<Home> {
             ),
             SizedBox(
               height: MediaQuery.sizeOf(context).height * 0.35,
-              child: FutureBuilder<Map<String, List<String>>>(
-                future: getData,
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(child: CircularProgressIndicator());
-                  } else if (snapshot.hasError) {
-                    return Center(child: Text('Error: ${snapshot.error}'));
-                  } else {
-                    List<String> totalTime = snapshot.data!['totalTime']!;
-                    List<double> totalTemperature = snapshot
-                        .data!['totalTemperature']!
-                        .map<double>((temp) => double.parse(temp))
-                        .toList();
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                child: FutureBuilder<Map<String, List<String>>>(
+                  future: getData,
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(child: CircularProgressIndicator());
+                    } else if (snapshot.hasError) {
+                      return Center(child: Text('Error: ${snapshot.error}'));
+                    } else {
+                      List<String> totalTime = snapshot.data!['totalTime']!;
+                      List<double> totalTemperature = snapshot
+                          .data!['totalTemperature']!
+                          .map<double>((temp) => double.parse(temp))
+                          .toList();
 
-                    chartData = [];
-                    for (int i = 0; i < totalTime.length; i++) {
-                      chartData.add(
-                          IndividualBar(((totalTime[i])), totalTemperature[i]));
+                      chartData = [];
+                      for (int i = 0; i < totalTime.length; i++) {
+                        chartData.add(IndividualBar(
+                            ((totalTime[i])), totalTemperature[i]));
+                      }
+                      return SfCartesianChart(
+                          backgroundColor: Colors.white,
+                          title: const ChartTitle(text: 'This Week Weather'),
+                          // primaryXAxis: DateTimeAxis(),
+                          enableAxisAnimation: true,
+                          primaryXAxis: const CategoryAxis(
+                              labelStyle: TextStyle(
+                                  // fontFamily: 'Roboto',
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w600),
+                              labelRotation: 90),
+                          primaryYAxis: const NumericAxis(
+                            edgeLabelPlacement: EdgeLabelPlacement.none,
+                            labelFormat: '{value}℃',
+                            axisLine: AxisLine(width: 0),
+                          ),
+                          series: <CartesianSeries>[
+                            // Renders line chart
+                            LineSeries<IndividualBar, String>(
+                              markerSettings:
+                                  const MarkerSettings(isVisible: true),
+                              color: const Color(0xFF06baab),
+                              dataSource: chartData,
+                              xValueMapper: (IndividualBar sales, _) =>
+                                  sales.day,
+                              yValueMapper: (IndividualBar sales, _) =>
+                                  sales.degree,
+                              dataLabelSettings:
+                                  const DataLabelSettings(isVisible: true),
+                            )
+                          ]);
                     }
-
-                    return SfCartesianChart(
-                        title: const ChartTitle(text: 'This Week Weather'),
-                        // primaryXAxis: DateTimeAxis(),
-                        enableAxisAnimation: true,
-                        primaryXAxis: const CategoryAxis(
-                            labelStyle: TextStyle(
-                                // fontFamily: 'Roboto',
-                                fontSize: 14,
-                                fontWeight: FontWeight.w600),
-                            labelRotation: 90),
-                        primaryYAxis: const NumericAxis(
-                          edgeLabelPlacement: EdgeLabelPlacement.none,
-                          labelFormat: '{value}℃',
-                          axisLine: AxisLine(width: 0),
-                        ),
-                        series: <CartesianSeries>[
-                          // Renders line chart
-                          LineSeries<IndividualBar, String>(
-                            markerSettings:
-                                const MarkerSettings(isVisible: true),
-                            color: const Color(0xFF06baab),
-                            dataSource: chartData,
-                            xValueMapper: (IndividualBar sales, _) => sales.day,
-                            yValueMapper: (IndividualBar sales, _) =>
-                                sales.degree,
-                            dataLabelSettings:
-                                const DataLabelSettings(isVisible: true),
-                          )
-                        ]);
-                  }
-                },
+                  },
+                ),
               ),
             ),
           ],
--- lib/model/getData.dart
+++ lib/model/getData.dart
@@ -5,7 +5,7 @@ import 'package:intl/intl.dart';
 
 class GetData {
   String api =
-      'https://api.open-meteo.com/v1/forecast?latitude=49.4295&longitude=1.0791&hourly=temperature_2m';
+      'https://api.open-meteo.com/v1/gfs?latitude=52.52&longitude=13.41&hourly=temperature_2m,relative_humidity_2m,visibility,wind_speed_10m';
 
   Future<Map<String, List<String>>> getData() async {
     http.Response response = await http.get(Uri.parse(api));
@@ -18,11 +18,13 @@ class GetData {
         var temperature = jsonDecode(data)['hourly']['temperature_2m'];
         List time = jsonDecode(data)['hourly']['time'];
 
+
         List<String> totalTime = [];
         List<String> totalTemperature = [];
 
+
         for (int i = 0; i < time.length; i++) {
-          if (i % 24 == 0) {
+          if (i % 24 == 11) {
             DateTime dateTime = DateTime.parse(time[i]);
             // String formattedDate = DateFormat('yyyy-MM-dd').format(dateTime);
             // String formattedDate = DateFormat('dd').format(dateTime);
@@ -30,11 +32,13 @@ class GetData {
 
             totalTime.add(formattedDate);
             totalTemperature.add(temperature[i].toString());
+
           }
         }
 
         ret['totalTime'] = totalTime;
         ret['totalTemperature'] = totalTemperature;
+
       } else {
         print(response.statusCode);
       }
@@ -60,9 +64,21 @@ class GetData {
 
         List<String> currentTime = [];
         List<String> currentTemp = [];
+        List<String> totalHumidity = [];
+        List<String> totalVisibility = [];
+        List<String> totalWind = [];
+
 
         List currentDataTime = jsonDecode(data)['hourly']['time'];
         var currentDataTemp = jsonDecode(data)['hourly']['temperature_2m'];
+
+
+        // Humidity / Wind / Visibility
+
+        var humidity = jsonDecode(data)['hourly']['relative_humidity_2m'];
+        var visibility = jsonDecode(data)['hourly']['visibility'];
+        var wind = jsonDecode(data)['hourly']['wind_speed_10m'];
+
         print(currentDataTemp);
 
         print(currentDataTime[2].substring(8, 10));
@@ -72,11 +88,19 @@ class GetData {
               (day == currentDataTime[i].substring(8, 10)))) {
             currentTime.add(currentDataTime[i]);
             currentTemp.add(currentDataTemp[i].toString());
+
+            totalHumidity.add(humidity[i].toString());
+            totalVisibility.add(visibility[i].toString());
+            totalWind.add(wind[i].toString());
           }
         }
 
         ret['currentDataTime'] = currentTime;
         ret['currentDataTemp'] = currentTemp;
+        ret['totalHumidity'] = totalHumidity;
+        ret['totalVisibility'] = totalVisibility;
+        ret['totalWind'] = totalWind;
+
       }
     } catch (e) {
       String message = 'Error fetching data because of: $e';
